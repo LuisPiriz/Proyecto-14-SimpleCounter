df3db8fe362dfb5afb051a7604941f66b5b45734
LuisPiriz <manupirizcorrea@gmail.com>
2023-01-09 15:16:00 +0000

    v1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,22 +4,14 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+const Home = (props) => {
+	return (<>
+		<div className="bg-dark text-info p-4">
+		<h1>Contador: <i class="fa fa-clock"></i> {props.numero6} {props.numero5} {props.numero4} {props.numero3} {props.numero2} {props.numero1}</h1></div>
+		{/* <div>
+		<input id="input" className="form-control bg-dark text-info w-25" type="number"></input>
+		<button id="button" className="btn btn-dark text-info">Inciar cuenta regresiva</button></div> */}
+		</>
 	);
 };
 
